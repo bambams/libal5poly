3a6eea10d41b574a1d81b79d4b5fafaaddc46a24
Brandon McCaig <bamccaig@gmail.com>
2012-12-20 01:07:31 -0500

    h4x: Add a simple Direction enum.

---
+++ include/al5poly/Direction.hpp
@@ -0,0 +1,14 @@
+#ifndef DIRECTION_HPP
+    #define DIRECTION_HPP
+
+namespace al5poly
+{
+    enum Direction
+    {
+        AL5POLY_DIRECTION_NONE,
+        AL5POLY_DIRECTION_LEFT,
+        AL5POLY_DIRECTION_RIGHT
+    }
+}
+
+#endif

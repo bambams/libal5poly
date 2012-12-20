ca76edadaefa5265aec2e13adfb580468fd95846
Brandon McCaig <bamccaig@gmail.com>
2012-12-20 03:11:59 -0500

    Add Orientation enum... To help with drawing.

---
+++ include/al5poly/Orientation.hpp
@@ -0,0 +1,14 @@
+#ifndef ORIENTATION_HPP
+    #define ORIENTATION_HPP
+
+namespace al5poly
+{
+    enum Orientation
+    {
+        AL5POLY_ORIENTATION_NONE,
+        AL5POLY_ORIENTATION_FLIPPED_HORIZONTAL,
+        AL5POLY_ORIENTATION_FLIPPED_VERTICAL
+    };
+}
+
+#endif
